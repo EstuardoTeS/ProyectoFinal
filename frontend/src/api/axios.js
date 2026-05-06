cae0ddef9bed8ddbde08c8b6916611c27b2a80b7
Estuardo Teni Sierra <tensierra25p@gmail.com>
2026-05-05 19:01:52 -0600

    Fix mobile APK API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
+import { Capacitor } from '@capacitor/core'
+
+const localApiUrl = 'http://127.0.0.1:8000/api'
+const productionApiUrl = 'https://techsolutions-backend.onrender.com/api'
+const baseURL = import.meta.env.VITE_API_URL || (Capacitor.isNativePlatform() ? productionApiUrl : localApiUrl)
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? 'http://127.0.0.1:8000/api',
+  baseURL,
 })
 
 api.interceptors.request.use((config) => {
